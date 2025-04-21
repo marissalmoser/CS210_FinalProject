abfc7685077efbe0944509dddb2ef3271ee69d0a
Marissa Moser <marissalmoser@gmail.com>
2025-04-20 19:22:51 -0500

    cache system functional

--- ProjectFiles/Milestone1/DriverProgram.cpp
+++ ProjectFiles/Milestone1/DriverProgram.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <vector>
+#include <unordered_map>
+#include <list>
 
 using namespace std;
 
-void SearchInCSV(string filename, string cc, string cn, bool hasHeader = true);
+string SearchInCSV(string filename, string cc, string cn, bool hasHeader = true);
+string SearchInCache(string cc, string cn);
+void StartSearch(string cc, string cn);
 void promptUser();
 
-struct City
-{
-    std::string countryCode;
-    std::string cityName;
-    int population;
-};
+list<pair<string, string>> cacheList;
+unordered_map<string, list<pair<string, string>>::iterator> cacheMap;
 
 int main()
 {
@@ -21,15 +20,57 @@ int main()
     promptUser();
 }
 
-void SearchInCSV(string filename, string cc, string cn, bool hasHeader)
+string SearchInCache(string cc, string cn)
+{
+    //check map for key
+    cout << "In cache(" << cacheList.size() << ")...\n";
+    string key = cn + "," + cc;
+    auto it = cacheMap.find(key);//- returns iterator pointing to pair in map
+
+    if (it != cacheMap.end())// - end iterator points to invalid 
+    {
+        //move node to front of list
+        cacheList.splice(cacheList.begin(), cacheList, it->second);
+
+        return it->second->second;
+    }
+    else
+    {
+        cout << "In csv file...\n";
+        string pop = SearchInCSV("world_cities.csv", cc, cn);
+
+        //checks if not found, prevents invalid data in cache
+        if (pop == "")
+        {
+            return pop;
+        }
+
+        //checks size of cache
+        if (cacheList.size() >= 10)
+        {
+            //remove last visited node from list and map
+            auto city = cacheList.back();
+            cacheMap.erase(city.first);
+            cacheList.pop_back();
+        }
+
+        //add new city to front of list and add pointer to map
+        cacheList.push_front({ key, pop });
+        cacheMap[key] = cacheList.begin();
+        return pop;
+    }
+}
+
+string SearchInCSV(string filename, string cc, string cn, bool hasHeader)
 {
     ifstream file(filename);
     string line, word;
     fstream fout;
+    string pop = "";
 
     if (!file.is_open()) {
         cerr << "Error: Could not open file " << filename << endl;
-        return;
+        return pop;
     }
 
     //check if first line should be skipped
@@ -59,14 +100,12 @@ void SearchInCSV(string filename, string cc, string cn, bool hasHeader)
         }
 
         //at this point, the country code and city name should match
-        string pop;
         getline(ss, pop, ',');
-        cout << "City found with population of " << pop << endl;
         break;
     }
 
     file.close();
-    promptUser();
+    return pop;
 }
 
 void promptUser()
@@ -82,11 +121,29 @@ void promptUser()
     cout <<
         "Input a City Name: \n    ";
 
-    cin >> cn;
+    cin.ignore();
+    getline(cin, cn);
 
     cout << "Searching...\n";
 
-    SearchInCSV("world_cities.csv", cc, cn);
+    StartSearch(cc, cn);
+}
 
+void StartSearch(string cc, string cn)
+{
+    string pop = SearchInCache(cc, cn);
+    
+    //validate data
+    if (pop == "")
+    {
+        cout << "City was not found\n";
+    }
+    else
+    {
+        cout << "City found with population of " << pop << "\n";
+    }
+    
     cout << "--------------------------------------\n" << endl;
+
+    promptUser();
 }
